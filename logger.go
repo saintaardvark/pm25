6092a4a2478cb66e785d1a69509aa982e4b9baeb
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2017-05-22 20:47:00 -0700

    Check for errors

--- logger.go
+++ logger.go
@@ -66,8 +66,11 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	measure, value := splitLine(reply)
 	fmt.Printf("%s: %s\n", measure, value)
+	measure, value, err := splitLine(reply)
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Next up: connecting to InfluxDB.")
 	// Create a new HTTPClient
 	ic, err := client.NewHTTPClient(client.HTTPConfig{
