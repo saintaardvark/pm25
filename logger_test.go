546f54b72f472059bc6af64b891d7c0a7b6b5dc6
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2017-05-25 20:43:20 -0700

    Rejig tests.
    Lots of repeated code, but works for now.

--- logger_test.go
+++ logger_test.go
@@ -1,26 +1,55 @@
 package main
 
 import (
-	"errors"
 	"testing"
 )
 
-func TestSplitLine(t *testing.T) {
-	var tests = []struct {
-		input string
-		want  Measurement
-		err   error
-	}{
-		{"{Humd: 33.10 %, }", Measurement{"Humid", 33.10, "%"}, nil},
-		{"{Temp: 22.70 C, }", Measurement{"Temp", 22.70, "C,"}, nil},
-		{"{Pres: 1007.03 hP, }", Measurement{"Pres", 1007.03, "hP"}, nil},
-		{"{Prcp: 0.00 NA, }", Measurement{"Prcp", 0, "NA"}, nil},
-		{"Waiting...", Measurement{"", 0, ""}, errors.New("Can't find colon in line, don't know how to split it")},
-		{"radio.new", Measurement{"", 0, ""}, errors.New("Can't find colon in line, don't know how to split it")},
-	}
-	for _, test := range tests {
+var testTable = []struct {
+	input string
+	want  Measurement
+	err   error
+}{
+	{"{Humd: 33.10 %}", Measurement{"Humd", 33.1, "%"}, nil},
+	{"{Temp: 22.70 C}", Measurement{"Temp", 22.7, "C"}, nil},
+	{"{Pres: 1007.03 hP}", Measurement{"Pres", 1007.03, "hP"}, nil},
+	{"{Prcp: 0.00 NA}", Measurement{"Prcp", 0, "NA"}, nil},
+	{"Waiting...", Measurement{"", 0, ""}, colonErr},
+	{"radio.new", Measurement{"", 0, ""}, colonErr},
+}
+
+func TestSplitLineName(t *testing.T) {
+	for _, test := range testTable {
 		got, err := SplitLine(test.input)
-		if test.want != got || err != test.err {
+		if test.want.Name != got.Name {
+			t.Errorf("SplitLine(%v) returned (%v, %v), want (%v, %v)",
+				test.input, got.Name, err, test.want.Name, test.err)
+		}
+	}
+}
+
+func TestSplitLineValue(t *testing.T) {
+	for _, test := range testTable {
+		got, err := SplitLine(test.input)
+		if test.want.Value != got.Value {
+			t.Errorf("SplitLine(%v) returned (%v, %v), want (%v, %v)",
+				test.input, got.Value, err, test.want.Value, test.err)
+		}
+	}
+}
+
+func TestSplitLineUnits(t *testing.T) {
+	for _, test := range testTable {
+		got, err := SplitLine(test.input)
+		if test.want.Units != got.Units {
+			t.Errorf("SplitLine(%v) returned (%v, %v), want (%v, %v)",
+				test.input, got.Units, err, test.want.Units, test.err)
+		}
+	}
+}
+func TestSplitLineError(t *testing.T) {
+	for _, test := range testTable {
+		got, err := SplitLine(test.input)
+		if test.err != err {
 			t.Errorf("SplitLine(%v) returned (%v, %v), want (%v, %v)",
 				test.input, got, err, test.want, test.err)
 		}
