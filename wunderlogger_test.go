e5f0376fa7315915b8a53b078338b06f0a945be8
Hugh Brown (Saint Aardvark the Carpeted) <aardvark@saintaardvarkthecarpeted.com>
2017-10-12 21:05:15 -0700

    Add some basic tests

---
+++ wunderlogger_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"testing"
+)
+
+var wl = wundergroundLogger{
+	apiKey:   "key",
+	endpoint: "https://wunder.example.com",
+	_name:    "test",
+}
+
+var wunderURLTestTable = []struct {
+	input Measurement
+	want  string
+	err   error
+}{
+	{Measurement{"Humd", 33.1, "%"}, "http://slashdot.org", nil},
+}
+
+func testBuildURL(t *testing.T) {
+	for _, test := range wunderURLTestTable {
+		got, err := wl.buildURL(test.input)
+		if test.want != got {
+			t.Errorf("buildURL(%v) returned (%v, %v), want (%v, %v)",
+				test.input, got, err, test.want, test.err)
+		}
+	}
+}
